b9e63d93ecf385d9b5fbc241f06c82e6f2691bcb
Wairia0 <Joseph.Wairia@strathmore.edu>
2025-09-29 01:15:54 +0300

    More templates

--- ecommerce/ecommerce/views.py
+++ ecommerce/ecommerce/views.py
@@ -2,7 +2,26 @@ from django.shortcuts import render
 
 
 def home(request):
+    context = {
+        
+        "page_title": "Home Page",
+        "user_name": "Wairia",
+    }
     return render(request, "home.html")
 
 def about(request):
     return render(request, "about.html")
+
+
+def dashboard(request):
+    context = {
+        "user_name": "Kamau",
+        "is_admin": True
+    }
+    return render(request, "dashboard.html")
+
+def shoppinglist(request):
+    context = {
+        "shoppinglist": ["Milk", "Eggs", "Butter"]
+    }
+    return render(request, "shoppinglist.html", context)
